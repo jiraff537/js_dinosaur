077afb810e8ed6249c0c12cf04b34ffb7ce5190a
jiraff537 <alexei.danilov@gmail.com>
2018-05-28 12:48:16 +0400

    webpack start working (but \dist\bundle.js in index.html)

---
+++ index.js
@@ -0,0 +1,3 @@
+// index.js
+var moment = require('moment');console.log("Hello from JavaScript npm and webpack!");
+console.log(moment().startOf('day').fromNow());
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+// webpack.config.js
+//webpack --display-error-details
+module.exports = {
+    mode: 'development',
+    entry: './index.js',
+    output: {
+      filename: 'bundle.js'
+    }
+  };
